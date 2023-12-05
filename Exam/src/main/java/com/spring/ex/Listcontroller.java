353e80b13493f23df0e3dc9134f9cdb3ebff31cb
Arcazam <qwer0469@naver.com>
2023-12-05 11:36:52 +0900

    master 11:36

--- Exam/src/main/java/com/spring/ex/Listcontroller.java
+++ Exam/src/main/java/com/spring/ex/Listcontroller.java
@@ -1,7 +1,8 @@
 package com.spring.ex;
 
 public class Listcontroller {
-	// masterListController
-	// masterListController
-	// masterListController
+	// masterListController	작성
+	// masterListController 수정
+	// masterListController 조회
+	// masterListController 삭제
 }
